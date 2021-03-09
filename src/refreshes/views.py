113026a7afe158d803095245a36d77cd7f6b791e
streonix <streonix@gmail.com>
2021-03-09 15:31:52 +0200

    some change

--- src/refreshes/views.py
+++ src/refreshes/views.py
@@ -4,4 +4,5 @@ from .models import PageRefresh
 
 
 def count(request):
+    PageRefresh.objects.create()
     return HttpResponse(f'This page was refreshed {PageRefresh.objects.create().id} times.')
